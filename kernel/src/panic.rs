68df8414812e5ce03445811e9ff7d52635ee2a5e
rsahwe <rsahwe@gmx.net>
2025-05-11 00:21:07 +0200

    improve panic handler with hlt inside loops

--- kernel/src/panic.rs
+++ kernel/src/panic.rs
@@ -1,30 +1,37 @@
 use core::{panic::PanicInfo, sync::atomic::{AtomicBool, Ordering}};
 
+use x86_64::instructions::{hlt, interrupts::disable};
+
+use crate::eprintln;
+
 static HAS_PANICKED: AtomicBool = AtomicBool::new(false);
 static HAS_PANICKED_AGAIN: AtomicBool = AtomicBool::new(false);
 
 #[panic_handler]
 fn kernel_panic(panic_info: &PanicInfo) -> ! {
-    use crate::eprintln;
-    use x86_64::instructions::interrupts::disable;
-
     disable();
 
     if HAS_PANICKED.load(Ordering::Relaxed) {
         if HAS_PANICKED_AGAIN.load(Ordering::Relaxed) {
-            loop {}
+            loop {
+                hlt();
+            }
         }
 
         HAS_PANICKED_AGAIN.store(true, Ordering::Relaxed);
 
         eprintln!("\nDOUBLE PANIC!!!\n{}", panic_info);
 
-        loop {}
+        loop {
+            hlt();
+        }
     }
 
     HAS_PANICKED.store(true, Ordering::Relaxed);
 
     eprintln!("\n{}", panic_info);
 
-    loop {}
+    loop {
+        hlt();
+    }
 }
