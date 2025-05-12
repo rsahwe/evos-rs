a1852a92fe16d80b77475f99df045739b17f672b
rsahwe <rsahwe@gmx.net>
2025-05-12 19:36:27 +0000

    Continued lexer desing + ident lexing

--- compiler/src/lexer.rs
+++ compiler/src/lexer.rs
@@ -1,15 +1,5 @@
 use core::{fmt::Display, iter::Peekable, marker::PhantomData, ops::{Add, AddAssign, Range, RangeInclusive}, str::CharIndices};
 
-/// All possible basic elements of a source file
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-pub enum Token<'src> {
-    /// Identifier: started by a..z|A..Z|_ and then same or 0..9
-    Ident(&'src str),
-
-    // Errors:
-    Error(LexerError<'src>),
-}
-
 /// Range in src (exclusive)
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct Span<'src> {
@@ -123,20 +113,47 @@ impl<'src> Display for DisplaySpan<'src> {
     }
 }
 
-/// Gives a reason for why the lexer stopped.
+/// All possible basic elements of a source file
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-pub enum LexerError<'src> {
-    MalformedInput(DisplaySpan<'src>),
-    UnexpectedEof(&'src str),
+pub enum Token<'src> {
+    /// Identifier: started by a..z|A..Z|_ and then same or 0..9
+    Ident(&'src str),
+
+    // Errors:
+    Error(LexerError),
 }
 
-impl<'src> Display for LexerError<'src> {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        match self {
-            LexerError::MalformedInput(display_span) => write!(f, "malformed input at:\n{}", display_span),
-            LexerError::UnexpectedEof(source) => write!(f, "unexpected Eof in {}", source),
-        }
+impl<'src> Token<'src> {
+    pub const fn add_span(self, span: Span<'src>) -> SpannedToken<'src> {
+        SpannedToken { token: self, span }
     }
+}
+
+/// Token + Span
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct SpannedToken<'src> {
+    pub token: Token<'src>,
+    pub span: Span<'src>,
+}
+
+/// Gives a reason for why the lexer stopped.
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum LexerError {
+    MalformedInput,
+    UnexpectedEof,
+}
+
+impl<'src> LexerError {
+    pub const fn add_span(self, span: Span<'src>) -> SpannedLexerError<'src> {
+        SpannedLexerError { error: self, span }
+    }
+}
+
+/// LexerError + Span
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct SpannedLexerError<'src> {
+    pub error: LexerError,
+    pub span: Span<'src>,
 }
 
 /// The iterator over tokens.
@@ -144,19 +161,55 @@ impl<'src> Display for LexerError<'src> {
 pub struct RawLexer<'src> {
     source: &'src str,
     chars: Peekable<CharIndices<'src>>,
+    complete: bool,
 }
 
 impl<'src> RawLexer<'src> {
     pub fn new(source: &'src str) -> Self {
-        Self { source, chars: source.char_indices().peekable() }
+        Self { source, chars: source.char_indices().peekable(), complete: false }
     }
 }
 
 impl<'src> Iterator for RawLexer<'src> {
-    type Item = Token<'src>;
+    type Item = SpannedToken<'src>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self.chars.next() {
+        let next = self.chars.next();
+
+        if next.is_none() {
+            if self.complete {
+                return None;
+            }
+
+            self.complete = true;
+
+            return Some(
+                Token::Error(LexerError::UnexpectedEof)
+                    .add_span(
+                        Span::new_single(self.source.len())
+                    )
+            );
+        }
+
+        let (pos, chr) = next.unwrap();
+
+        match chr {
+            c if c.is_whitespace() => {
+                self.next()
+            },
+            c if c.is_alphabetic() || c == '_' => {
+                let start = pos;
+
+                let mut end = start;
+
+                while let Some(next) = self.chars.next_if(|n| n.1.is_alphanumeric() || n.1 == '_') {
+                    end = next.0;
+                }
+
+                //TODO: KEYWORDS
+
+                Some(Token::Ident(&self.source[start..=end]).add_span(Span::new_inclusive(start..=end)))
+            },
             _ => todo!("Lexer for `{}`!!!", self.source)
         }
     }
@@ -166,20 +219,20 @@ impl<'src> Iterator for RawLexer<'src> {
 #[derive(Clone, Debug)]
 pub struct Lexer<'src> {
     inner: Peekable<RawLexer<'src>>,
-    end_error: Option<LexerError<'src>>,
+    end_error: Option<SpannedLexerError<'src>>,
 }
 
 impl<'src> Lexer<'src> {
-    pub fn next(&mut self) -> Result<Token<'src>, LexerError<'src>> {
+    pub fn next(&mut self) -> Result<SpannedToken<'src>, SpannedLexerError<'src>> {
         match self.end_error {
             Some(error) => Err(error),
             None => {
                 match self.inner.next() {
                     Some(next) => {
-                        match next {
+                        match next.token {
                             Token::Error(error) => {
-                                self.end_error = Some(error);
-                                Err(error)
+                                self.end_error = Some(error.add_span(next.span));
+                                Err(error.add_span(next.span))
                             },
                             _ => Ok(next)
                         }
@@ -190,16 +243,16 @@ impl<'src> Lexer<'src> {
         }
     }
 
-    pub fn peek(&mut self) -> Result<Token<'src>, LexerError<'src>> {
+    pub fn peek(&mut self) -> Result<SpannedToken<'src>, SpannedLexerError<'src>> {
         match self.end_error {
             Some(error) => Err(error),
             None => {
                 match self.inner.peek() {
                     Some(next) => {
-                        match next {
+                        match next.token {
                             Token::Error(error) => {
-                                self.end_error = Some(*error);
-                                Err(*error)
+                                self.end_error = Some(error.add_span(next.span));
+                                Err(error.add_span(next.span))
                             },
                             _ => Ok(*next)
                         }
@@ -214,6 +267,10 @@ impl<'src> Lexer<'src> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    extern crate std;
+    use std::prelude::rust_2024::*;
+    use std::*;
 
     const SPAN_TEST_SLICE: &str = "This is a test\nLine two\nEOF:";
 
@@ -237,5 +294,17 @@ mod tests {
         assert_eq!(Span::new_exclusive(2..3) + Span::new_exclusive(0..1), Span::new_exclusive(0..3));
         assert_eq!(Span::new_exclusive(2..3) + Span::new_exclusive(0..3), Span::new_exclusive(0..3));
         assert_eq!(Span::new_exclusive(2..3) + Span::new_exclusive(0..1), Span::new_exclusive(0..3));
+    }
+
+    #[test]
+    fn raw_lexer() {
+        macro_rules! test_lexer_output {
+            ($input:expr, $output:expr, $msg:expr) => {
+                assert!(RawLexer::new($input).map(|st| dbg!(st.token)).eq($output), $msg)
+            };
+        }
+
+        test_lexer_output!("  test\n ", [Token::Ident("test"), Token::Error(LexerError::UnexpectedEof)].into_iter(), "Simple ident test");
+        test_lexer_output!("  te st\n ", [Token::Ident("te"), Token::Ident("st"), Token::Error(LexerError::UnexpectedEof)].into_iter(), "Simple ident test 2");
     }
 }
