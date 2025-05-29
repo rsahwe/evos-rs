027c0dc45573f5150c5095b78c153e41c7874244
rsahwe <rsahwe@gmx.net>
2025-05-29 22:45:19 +0200

    Timeout convenience methods

--- kernel/src/time.rs
+++ kernel/src/time.rs
@@ -1,4 +1,4 @@
-use core::sync::atomic::{AtomicU16, AtomicU64, Ordering};
+use core::{hint::spin_loop, sync::atomic::{AtomicU16, AtomicU64, Ordering}};
 
 use crate::interrupts::PicEnd;
 
@@ -11,6 +11,32 @@ pub struct Time {}
 impl Time {
     pub fn boot_time_ns() -> u64 {
         BOOT_NS.load(Ordering::Relaxed)
+    }
+
+    pub fn timeout_poll_ns<F: FnMut() -> bool>(timeout_ns: u64, mut poll: F) -> bool {
+        let end_time_ns = Self::boot_time_ns() + timeout_ns;
+
+        while Self::boot_time_ns() <= end_time_ns {
+            if poll() {
+                return true;
+            }
+
+            spin_loop();
+        }
+
+        false
+    }
+
+    pub fn timeout_poll_us<F: FnMut() -> bool>(timeout_us: u64, poll: F) -> bool {
+        Self::timeout_poll_ns(timeout_us * 1000, poll)
+    }
+
+    pub fn timeout_poll_ms<F: FnMut() -> bool>(timeout_ms: u64, poll: F) -> bool {
+        Self::timeout_poll_us(timeout_ms * 1000, poll)
+    }
+
+    pub fn timeout_poll_s<F: FnMut() -> bool>(timeout_s: u64, poll: F) -> bool {
+        Self::timeout_poll_ms(timeout_s * 1000, poll)
     }
 
     pub(crate) fn set_ps_tick_step(step: u64) {
