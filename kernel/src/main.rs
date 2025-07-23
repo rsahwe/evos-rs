ec203c49cf2e969b693b608a785a80e6066fb276
rsahwe <rsahwe@gmx.net>
2025-07-23 18:15:14 +0200

    Rebase fix

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -2,12 +2,14 @@
 #![no_main]
 
 use bootloader_api::BootInfo;
-use evkrnl::{BOOT_CONFIG, init};
+use evkrnl::{init, BOOT_CONFIG, syscall_test};
 
 bootloader_api::entry_point!(kernel_main, config = &BOOT_CONFIG);
 
 fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
     init(boot_info);
+
+    syscall_test();
 
     panic!("Kernel main exited!")
 }
